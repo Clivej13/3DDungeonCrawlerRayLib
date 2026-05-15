bba4ada7196ca260044714cf6e270bff4ab7e881
Clivej13 <jamesclive13@gmail.com>
2026-05-15 10:39:18 +0100

    Initial Raylib C# project setup

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        const int screenWidth = 1280;
+        const int screenHeight = 720;
+
+        Raylib.InitWindow(screenWidth, screenHeight, "My Raylib Game");
+
+        Raylib.SetTargetFPS(60);
+
+        Vector2 playerPos = new Vector2(400, 300);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            // Movement
+            if (Raylib.IsKeyDown(KeyboardKey.Right))
+                playerPos.X += 5;
+
+            if (Raylib.IsKeyDown(KeyboardKey.Left))
+                playerPos.X -= 5;
+
+            if (Raylib.IsKeyDown(KeyboardKey.Up))
+                playerPos.Y -= 5;
+
+            if (Raylib.IsKeyDown(KeyboardKey.Down))
+                playerPos.Y += 5;
+
+            // Drawing
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.Black);
+
+            Raylib.DrawText("Raylib + C#", 20, 20, 30, Color.White);
+
+            Raylib.DrawCircleV(playerPos, 30, Color.Red);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
